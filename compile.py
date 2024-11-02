7108f5488787a9591e70e145c0fd6b7b96873970
Kim Dong-Hyun <temeddix@gmail.com>
2024-11-02 14:33:40 +0900

    Compile with proper configs

--- compile.py
+++ compile.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 command = [
     "wasm-pack",
@@ -10,5 +11,13 @@ command = [
     "../public",
     "--no-typescript",
     "--no-package",
+    "--",
+    "-Z",
+    "build-std=std,panic_abort",
 ]
-subprocess.run(command)
+env = {
+    "RUSTUP_TOOLCHAIN": "nightly",
+    "RUSTFLAGS": "-C target-feature=+atomics,+bulk-memory,+mutable-globals",
+}
+env.update(os.environ)
+subprocess.run(command, env=env)
