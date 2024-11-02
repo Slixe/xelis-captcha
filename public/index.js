5143c1eff1ad50b5e6286bc74609f0e045ed832b
Slixe <slixeprivate@gmail.com>
2024-11-02 10:43:34 +0100

    estimate hashrate

--- public/index.js
+++ public/index.js
@@ -1,7 +1,10 @@
-import init, { hello_world } from "/xelis_captcha.js";
+import init, { hello_world, estimate_hashrate } from "/xelis_captcha.js";
 
 console.log("Loading WASM module...");
 await init();
 console.log("WASM module loaded!");
 let msg = await hello_world();
 console.log("Message: ", msg);
+console.log("Estimating hashrate...");
+let hashrate = await estimate_hashrate(100, navigator.hardwareConcurrency);
+console.log("Hashrate: ", hashrate);
--- xelis-captcha/src/lib.rs
+++ xelis-captcha/src/lib.rs
@@ -1,5 +1,30 @@
+use rand::{rngs::OsRng, RngCore};
 use tokio_with_wasm::task::spawn_blocking;
+use xelis_common::{block::{Algorithm, BLOCK_WORK_SIZE}, crypto};
 use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+pub async fn estimate_hashrate(iterations: u32, n_threads: u32) -> u32 {
+    let mut handles = Vec::new();
+    let start = web_time::Instant::now();
+    for _ in 0..n_threads {
+        let handle = spawn_blocking(move || {
+            let mut input = [0u8; BLOCK_WORK_SIZE];
+            OsRng.fill_bytes(&mut input);
+            for _ in 0..iterations {
+                let _ = crypto::pow_hash(&input, Algorithm::V2).unwrap();
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    let elapsed = start.elapsed().as_secs();
+    (iterations * n_threads) / elapsed as u32
+}
 
 #[wasm_bindgen]
 pub async fn hello_world() -> String {
