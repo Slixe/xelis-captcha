9b45da069fc060636e78f084bbfadea68b4d5af3
Slixe <slixeprivate@gmail.com>
2024-11-03 18:31:13 +0100

    estimate hashrate

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import init, { hello_world, estimate_hashrate } from "/xelis_captcha.js";
+import init, { hello_world, estimate_hashrate, estimate_hashrate2 } from "/xelis_captcha.js";
 
 console.log("Loading WASM module...");
 await init();
@@ -6,5 +6,18 @@ console.log("WASM module loaded!");
 let msg = await hello_world();
 console.log("Message: ", msg);
 console.log("Estimating hashrate...");
-let hashrate = await estimate_hashrate(100, navigator.hardwareConcurrency);
-console.log("Hashrate: ", hashrate);
+
+document.body.appendChild(document.createTextNode("Starting..."));
+
+let hashrate = await estimate_hashrate2(100);
+document.body.appendChild(document.createTextNode("Hashrate: " + hashrate));
+
+try {
+    hashrate = await estimate_hashrate(100, navigator.hardwareConcurrency);
+    console.log("Hashrate (multi-threads): ", hashrate);
+    document.body.appendChild(document.createTextNode("Hashrate (" + navigator.hardwareConcurrency + "): " + hashrate));
+} catch (e) {
+    console.error(e);
+    document.body.appendChild(document.createTextNode("Error: " + e));
+}
+
--- xelis-captcha/src/lib.rs
+++ xelis-captcha/src/lib.rs
@@ -1,10 +1,14 @@
 use rand::{rngs::OsRng, RngCore};
 use tokio_with_wasm::task::spawn_blocking;
-use xelis_common::{block::{Algorithm, BLOCK_WORK_SIZE}, crypto};
+use xelis_common::{
+    block::{Algorithm, BLOCK_WORK_SIZE},
+    crypto,
+    utils::format_hashrate
+};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub async fn estimate_hashrate(iterations: u32, n_threads: u32) -> u32 {
+pub async fn estimate_hashrate(iterations: u32, n_threads: u32) -> String {
     let mut handles = Vec::new();
     let start = web_time::Instant::now();
     for _ in 0..n_threads {
@@ -22,8 +26,22 @@ pub async fn estimate_hashrate(iterations: u32, n_threads: u32) -> u32 {
         handle.await.unwrap();
     }
 
-    let elapsed = start.elapsed().as_secs();
-    (iterations * n_threads) / elapsed as u32
+    let elapsed = start.elapsed().as_millis();
+    format_hashrate(1000f64 / (elapsed as f64 / (n_threads*iterations) as f64))
+}
+
+#[wasm_bindgen]
+pub async fn estimate_hashrate2(iterations: u32) -> String {
+    let start = web_time::Instant::now();
+
+    let mut input = [0u8; BLOCK_WORK_SIZE];
+    OsRng.fill_bytes(&mut input);
+    for _ in 0..iterations {
+        let _ = crypto::pow_hash(&input, Algorithm::V2).unwrap();
+    }
+
+    let elapsed = start.elapsed().as_millis();
+    format_hashrate(1000f64 / (elapsed as f64 / iterations as f64))
 }
 
 #[wasm_bindgen]
